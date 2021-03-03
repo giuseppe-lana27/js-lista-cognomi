4ce615915fb01d29392f9467d0dbb560407d3346
Giuseppe Lana <giuseppe.lana27@outlook.it>
2021-03-03 16:34:04 +0100

    modifica commenti nel file javascript

--- js/script.js
+++ js/script.js
@@ -6,9 +6,11 @@ scrivi anche la posizione "umana" della lista in cui il nuovo utente si trova */
 var cognomi = ["Bianchi", "Rossi", "Duzioni", "Balsano", "Verdi"];
 // 2. chiedo all'utente di inserire il suo cognome
 var cognomeUtente = prompt("Inserisci il tuo cognome");
+// inserisco il cognome digitato nell'array
 cognomi.push(cognomeUtente);
+// metto in ordine alfabetico i cognomi dell'array
 console.log(cognomi.sort());
-// 3. cerco l'indice della posizione del cognome inserito
+// 3. cerco l'indice della posizione del cognome inserito e lo stampo in pagina
 for (var i = 0; i < cognomi.length; i++){
   console.log((cognomi.indexOf(cognomeUtente) + 1));
   document.getElementById('lista-cognomi').innerHTML += "<li>" + cognomi[i] + "</li>";
